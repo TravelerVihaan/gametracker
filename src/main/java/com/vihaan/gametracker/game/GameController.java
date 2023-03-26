71c8b50314f19990d239401abe632fdcf20cfdef
travelervihaan <traveler.vihaan@gmail.com>
2023-03-26 22:15:22 +0200

    Define model #1

--- src/main/java/com/vihaan/gametracker/game/GameController.java
+++ src/main/java/com/vihaan/gametracker/game/GameController.java
@@ -1,9 +1,25 @@
 package com.vihaan.gametracker.game;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
 
 @RestController("/games")
 public class GameController {
 
+    @GetMapping
+    public ResponseEntity<List<Game>> getAllGames(){
+        return ResponseEntity.ok(Collections.emptyList());
+    }
 
+    @PostMapping
+    public ResponseEntity<Game> addGame(@RequestBody Game game){
+        //TODO
+        return ResponseEntity.ok(game);
+    }
 }
