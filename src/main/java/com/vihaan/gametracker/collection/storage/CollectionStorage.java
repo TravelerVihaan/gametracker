26c4194906f8dba5f14f73b67f8dec8bd86ce664
TravelerVihaan <32998032+TravelerVihaan@users.noreply.github.com>
2023-03-27 19:38:55 +0200

    Attach storage to keep results #2

--- src/main/java/com/vihaan/gametracker/collection/storage/CollectionStorage.java
+++ src/main/java/com/vihaan/gametracker/collection/storage/CollectionStorage.java
@@ -12,14 +12,22 @@ import java.util.Set;
 
 public interface CollectionStorage {
 
-    GamesCollection getGamesCollection(String collectionName);
-
     GamesCollection getSortedGamesCollection(String collectionName, SortType sortType);
 
-    List<GameCollectionElement> getGamesFromCollection(String collectionName);
-    List<GameCollectionElement> getCustiomizedGamesFromCollection(String collectionName, Set<GamesCollectionFilter> filters, SortType sortType);
+    default GamesCollection getGamesCollection(String collectionName){
+        return getSortedGamesCollection(collectionName, SortType.ASCENDING);
+    };
+    default List<GameCollectionElement> getGamesFromCollection(String collectionName){
+        return  getCustomizedGamesFromCollection(collectionName, Collections.emptySet(), SortType.ASCENDING);
+    };
 
-    default List<GameCollectionElement> applyFilters(Set<GamesCollectionFilter> filters, List<GameCollectionElement> games) {
+    default List<GameCollectionElement> getCustomizedGamesFromCollection(String collectionName, Set<GamesCollectionFilter> filters, SortType sortType){
+        var gamesCollection = getGamesCollection(collectionName).games();
+        gamesCollection.sort(sortType.getGamesComparator());
+        return filters.isEmpty() ? gamesCollection : applyFilters(filters, gamesCollection);
+    };
+
+    private List<GameCollectionElement> applyFilters(Set<GamesCollectionFilter> filters, List<GameCollectionElement> games) {
         List<GameCollectionElement> filteredGames = new ArrayList<>(games);
         for(GamesCollectionFilter gamesFilter : filters) {
             filteredGames = filteredGames.stream().filter(gamesFilter::verify).toList();
--- src/main/java/com/vihaan/gametracker/collection/storage/sql/CollectionSqlStorage.java
+++ src/main/java/com/vihaan/gametracker/collection/storage/sql/CollectionSqlStorage.java
@@ -1,18 +1,13 @@
 package com.vihaan.gametracker.collection.storage.sql;
 
 import com.vihaan.gametracker.ModelElementMapper;
-import com.vihaan.gametracker.collection.GameCollectionElement;
 import com.vihaan.gametracker.collection.GamesCollection;
-import com.vihaan.gametracker.collection.GamesCollectionFilter;
 import com.vihaan.gametracker.collection.SortType;
 import com.vihaan.gametracker.collection.storage.CollectionStorage;
 import com.vihaan.gametracker.collection.storage.sql.model.GamesCollectionEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
 
 @Service
 public class CollectionSqlStorage implements CollectionStorage {
@@ -22,31 +17,16 @@ public class CollectionSqlStorage implements CollectionStorage {
 
     @Autowired
     public CollectionSqlStorage(GamesCollectionRepository gamesCollectionRepository,
-                                ModelElementMapper<GamesCollection, GamesCollectionEntity> gamesCollectionMapper) {
+                                @Qualifier("games_collection_mapper") ModelElementMapper<GamesCollection, GamesCollectionEntity> gamesCollectionMapper) {
         this.gamesCollectionRepository = gamesCollectionRepository;
         this.gamesCollectionMapper = gamesCollectionMapper;
     }
 
-    public GamesCollection getGamesCollection(String collectionName){
-        return getSortedGamesCollection(collectionName, SortType.ASCENDING);
-    }
     public GamesCollection getSortedGamesCollection(String collectionName, SortType sortType){
         GamesCollection gamesCollection = gamesCollectionRepository.getGameCollectionByCollectionName(collectionName)
                 .map(gamesCollectionMapper::convertToDomainObject)
                 .orElseGet(GamesCollection::emptyGamesCollection);
         gamesCollection.games().sort(sortType.getGamesComparator());
         return gamesCollection;
-    }
-
-    @Override
-    public List<GameCollectionElement> getGamesFromCollection(String collectionName) {
-        return  getCustiomizedGamesFromCollection(collectionName, Collections.emptySet(), SortType.ASCENDING);
-    }
-
-    @Override
-    public List<GameCollectionElement> getCustiomizedGamesFromCollection(String collectionName, Set<GamesCollectionFilter> filters, SortType sortType) {
-        var gamesCollection = getGamesCollection(collectionName).games();
-        gamesCollection.sort(sortType.getGamesComparator());
-        return filters.isEmpty() ? gamesCollection : applyFilters(filters, gamesCollection);
     }
 }
